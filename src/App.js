d60b78a1fb502adcb11469c78d9a08df7c9e67e4
summer-bonobono <fmadudid@gmail.com>
2022-01-20 23:40:16 +0900

    react와 express 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/api/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ title: data.title }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            {this.state.title ? (
+              <h1>{this.state.title}</h1>
+            ) : (
+              <h1>loading...</h1>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
