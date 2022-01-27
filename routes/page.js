1de32acf28e559b66a456e7c4b713f8e581e4205
narongkim <kyj0032@korea.ac.kr>
2022-01-27 14:27:22 +0900

    /myroom 라우터 작성, ref값 추가

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-
+const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -8,21 +8,40 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get("/", (req, res, next) => {
-  res.render("main", {
-    title: "hi",
+router.get('/', (req, res, next) => {
+  res.render('main', {
+    title: 'hi',
   });
 });
 
-router.get("/logout", isLoggedIn, (req, res) => {
+router.get('/logout', isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
-  res.redirect("/");
+  res.redirect('/');
 });
 
-router.get("/myroom", (req, res) => {
-  // isLoggedIn
-  res.render("myroom");
+router.get('/myroom', async (req, res) => {
+  var url = req.url; // /myroom/?ref=...
+  var arr = url.split('='); // [/myroom/?ref, ref값]
+
+  if (arr.length == 1) {
+    // /myroom
+    if (req.user) {
+      res.redirect('/myroom/?ref=' + req.user.ref);
+    } else {
+      res.redirect('/');
+    }
+  } else {
+    try {
+      const host = await User.findOne({
+        where: { ref: arr[1] },
+      });
+      res.render('myroom', { hostref: host });
+    } catch (error) {
+      console.error(error);
+      return next(error);
+    }
+  }
 });
 
 module.exports = router;
