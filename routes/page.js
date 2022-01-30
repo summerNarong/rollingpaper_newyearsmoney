bf9a1c4e65d7ba6e6d19458a06dec26fd37b7e90
summer-bonobono <fmadudid@gmail.com>
2022-01-30 12:30:16 +0900

    마지막창 구현 및 라우터 연결, asset계산 시도

--- routes/page.js
+++ routes/page.js
@@ -1,45 +1,51 @@
-const express = require('express');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const User = require('../models/user');
-const Message = require('../models/message');
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const User = require("../models/user");
+const Message = require("../models/message");
+const Image = require("../models/image");
 const router = express.Router();
+const sequelize = require("sequelize");
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
 
-router.get('/', (req, res, next) => {
-  res.render('main', {
-    title: 'hi',
+router.get("/", (req, res, next) => {
+  res.render("main", {
+    title: "hi",
   });
 });
 
-router.get('/logout', isLoggedIn, (req, res) => {
+router.get("/logout", isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.get('/myroom', async (req, res) => {
+router.get("/myroom", async (req, res) => {
   var url = req.url; // /myroom/?ref=...
-  var arr = url.split('='); // [/myroom/?ref, ref값]
+  var arr = url.split("="); // [/myroom/?ref, ref값]
 
   if (arr.length == 1) {
     // /myroom
     if (req.user) {
-      res.redirect('/myroom/?ref=' + req.user.ref);
+      res.redirect("/myroom/?ref=" + req.user.ref);
     } else {
-      res.redirect('/');
+      res.redirect("/");
     }
   } else {
     try {
       const host = await User.findOne({
         where: { ref: arr[1] },
       });
-      res.render('myroom', {
+      const msgs = await Message.findAll({
+        where: { ref: arr[1] },
+      });
+      res.render("myroom", {
         hostuser: host,
-        url: '/sendmsg/?ref=' + host.ref,
+        url: "/sendmsg/?ref=" + host.ref,
+        msg: msgs,
       });
     } catch (error) {
       console.error(error);
@@ -48,16 +54,16 @@ router.get('/myroom', async (req, res) => {
   }
 });
 
-router.get('/sendmsg', (req, res) => {
+router.get("/sendmsg", (req, res) => {
   var url = req.url; // /myroom/?ref=...
-  var arr = url.split('='); // [/myroom/?ref, ref값]
-  res.render('sendmsg', { url: '/post/?ref=' + arr[1] });
+  var arr = url.split("="); // [/myroom/?ref, ref값]
+  res.render("sendmsg", { url: "/post/?ref=" + arr[1] });
 });
 
-router.post('/post', async (req, res) => {
+router.post("/post", async (req, res) => {
   try {
     var url = req.url; // /myroom/?ref=...
-    var arr = url.split('=')[1]; // [/myroom/?ref, ref값]
+    var arr = url.split("=")[1]; // [/myroom/?ref, ref값]
 
     const post = await Message.create({
       sender: req.body.sendername,
@@ -65,9 +71,53 @@ router.post('/post', async (req, res) => {
       ref: arr,
       imageId: req.body.money,
     });
-    res.redirect('/');
+
+    // var assetValue = await User.findOne({
+    //   attributes: ['asset'],
+    //   where: {ref: arr},
+    // });
+    // var moneyValue = await Image.findOne({
+    //   attributes: ['price'],
+    //   where: {id: req.body.money},
+    // });
+    // assetValue.asset += moneyValue.price;
+    const assetValue = await Message.findAll({
+      include: [
+        {
+          model: Image,
+          // attributes: ["id", "price"],
+        },
+      ],
+      where: {
+        ref: arr,
+      },
+      //attributes: [[sequelize.fn("sum", sequelize.col("price")), total]],
+      //group: [Message.ref],
+    });
+    console.log(assetValue);
+    // var result;
+    // assetValue.sum("price").then((sum) => {
+    //   result = sum;
+    // });
+    // console.log(result);
+    /*
+    User.update(
+      {
+        asset: assetValue[0].total,
+      },
+      {
+        where: { ref: arr },
+      }
+    );*/
+
+    //assetValue.sum('price');
+    res.redirect("/complete");
   } catch (error) {
     console.error(error);
   }
+});
+
+router.get("/complete", (req, res) => {
+  res.render("complete");
 });
 module.exports = router;
