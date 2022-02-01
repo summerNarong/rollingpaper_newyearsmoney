26ae228c81c16597864d90f99c403417645f8262
narongkim <kyj0032@korea.ac.kr>
2022-02-01 23:00:55 +0900

    /post에서 user.asset에 합 저장

--- routes/page.js
+++ routes/page.js
@@ -1,50 +1,54 @@
-const express = require("express");
-const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const User = require("../models/user");
-const Message = require("../models/message");
-const Image = require("../models/image");
+const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+const Message = require('../models/message');
+const Image = require('../models/image');
 const router = express.Router();
-const sequelize = require("sequelize");
+const sequelize = require('sequelize');
+//const { QueryTypes } = require('sequelize');
+const db = require('../models/index.js');
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
 
-router.get("/", (req, res, next) => {
-  res.render("main", {
-    title: "hi",
+router.get('/', (req, res, next) => {
+  res.render('main', {
+    title: 'hi',
   });
 });
 
-router.get("/logout", isLoggedIn, (req, res) => {
+router.get('/logout', isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
-  res.redirect("/");
+  res.redirect('/');
 });
 
-router.get("/myroom", async (req, res) => {
+router.get('/myroom', async (req, res, next) => {
   var url = req.url; // /myroom/?ref=...
-  var arr = url.split("="); // [/myroom/?ref, ref값]
+  var arr = url.split('='); // [/myroom/?ref, ref값]
+  var refValue = arr[1];
 
   if (arr.length == 1) {
     // /myroom
     if (req.user) {
-      res.redirect("/myroom/?ref=" + req.user.ref);
+      res.redirect('/myroom/?ref=' + req.user.ref);
     } else {
-      res.redirect("/");
+      res.redirect('/');
     }
   } else {
     try {
       const host = await User.findOne({
-        where: { ref: arr[1] },
+        where: { ref: refValue },
       });
       const msgs = await Message.findAll({
-        where: { ref: arr[1] },
+        where: { ref: refValue },
       });
-      res.render("myroom", {
+
+      res.render('myroom', {
         hostuser: host,
-        url: "/sendmsg/?ref=" + host.ref,
+        url: '/sendmsg/?ref=' + host.ref,
         msg: msgs,
       });
     } catch (error) {
@@ -54,70 +58,50 @@ router.get("/myroom", async (req, res) => {
   }
 });
 
-router.get("/sendmsg", (req, res) => {
+router.get('/sendmsg', (req, res) => {
   var url = req.url; // /myroom/?ref=...
-  var arr = url.split("="); // [/myroom/?ref, ref값]
-  res.render("sendmsg", { url: "/post/?ref=" + arr[1] });
+  var refValue = url.split('=')[1]; // [/myroom/?ref, ref값]
+  res.render('sendmsg', { url: '/post/?ref=' + refValue });
 });
 
-router.post("/post", async (req, res) => {
+router.post('/post', async (req, res) => {
   try {
     var url = req.url; // /myroom/?ref=...
-    var arr = url.split("=")[1]; // [/myroom/?ref, ref값]
+    var refValue = url.split('=')[1]; // [/myroom/?ref, ref값]
 
     const post = await Message.create({
       sender: req.body.sendername,
       textmsg: req.body.content,
-      ref: arr,
+      ref: refValue,
       imageId: req.body.money,
     });
 
-    // var assetValue = await User.findOne({
-    //   attributes: ['asset'],
-    //   where: {ref: arr},
-    // });
-    // var moneyValue = await Image.findOne({
-    //   attributes: ['price'],
-    //   where: {id: req.body.money},
-    // });
-    // assetValue.asset += moneyValue.price;
-    const assetValue = await Message.findAll({
-      include: [
-        {
-          model: Image,
-          // attributes: ["id", "price"],
-        },
-      ],
-      where: {
-        ref: arr,
-      },
-      //attributes: [[sequelize.fn("sum", sequelize.col("price")), total]],
-      //group: [Message.ref],
+    let query =
+      'select sum(price) as total from Message join Image on Message.imageId=Image.id where Message.ref=:ref';
+    let sum = await db.sequelize.query(query, {
+      replacements: { ref: refValue },
+      type: db.sequelize.QueryTypes.SELECT,
+      raw: true,
     });
-    console.log(assetValue);
-    // var result;
-    // assetValue.sum("price").then((sum) => {
-    //   result = sum;
-    // });
-    // console.log(result);
-    /*
+
+    console.log(sum[0].total);
+
     User.update(
       {
-        asset: assetValue[0].total,
+        asset: sum[0].total,
       },
       {
-        where: { ref: arr },
+        where: { ref: refValue },
       }
-    );*/
+    );
 
-    //assetValue.sum('price');
-    res.redirect("/complete");
+    res.redirect('/complete');
   } catch (error) {
     console.error(error);
   }
 });
 
-router.get("/complete", (req, res) => {
-  res.render("complete");
+router.get('/complete', (req, res) => {
+  res.render('complete');
 });
 module.exports = router;
