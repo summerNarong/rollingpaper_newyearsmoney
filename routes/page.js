5b868b7b5818e392d025de1742d23cd5b8dd9e85
summer-bonobono <fmadudid@gmail.com>
2022-02-17 15:26:16 +0900

    모달 완성,myroom에 프로필 데이터 불러오기

--- routes/page.js
+++ routes/page.js
@@ -1,41 +1,42 @@
-const express = require('express');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const User = require('../models/user');
-const Message = require('../models/message');
-const Image = require('../models/image');
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const User = require("../models/user");
+const Message = require("../models/message");
+const Image = require("../models/image");
 const router = express.Router();
-const sequelize = require('sequelize');
+const sequelize = require("sequelize");
+const dayjs = require("dayjs");
 //const { QueryTypes } = require('sequelize');
-const db = require('../models/index.js');
+const db = require("../models/index.js");
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
 
-router.get('/', (req, res, next) => {
-  res.render('main', {
-    title: 'hi',
+router.get("/", (req, res, next) => {
+  res.render("main", {
+    title: "hi",
   });
 });
 
-router.get('/logout', isLoggedIn, (req, res) => {
+router.get("/logout", isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.get('/myroom', async (req, res, next) => {
+router.get("/myroom", async (req, res, next) => {
   var url = req.url; // /myroom/?ref=...
-  var arr = url.split('='); // [/myroom/?ref, ref값]
+  var arr = url.split("="); // [/myroom/?ref, ref값]
   var refValue = arr[1];
 
   if (arr.length == 1) {
     // /myroom
     if (req.user) {
-      res.redirect('/myroom/?ref=' + req.user.ref);
+      res.redirect("/myroom/?ref=" + req.user.ref);
     } else {
-      res.redirect('/');
+      res.redirect("/");
     }
   } else {
     try {
@@ -45,11 +46,14 @@ router.get('/myroom', async (req, res, next) => {
       const msgs = await Message.findAll({
         where: { ref: refValue },
       });
-
-      res.render('myroom', {
+      var date = dayjs(host.createdAt);
+      date = date.format("YYYY년 MM월 DD일");
+      console.log(msgs);
+      res.render("myroom", {
         hostuser: host,
-        url: '/sendmsg/?ref=' + host.ref,
+        url: "/sendmsg/?ref=" + host.ref,
         msg: msgs,
+        date: date,
       });
     } catch (error) {
       console.error(error);
@@ -58,16 +62,16 @@ router.get('/myroom', async (req, res, next) => {
   }
 });
 
-router.get('/sendmsg', (req, res) => {
+router.get("/sendmsg", (req, res) => {
   var url = req.url; // /myroom/?ref=...
-  var refValue = url.split('=')[1]; // [/myroom/?ref, ref값]
-  res.render('sendmsg', { url: '/post/?ref=' + refValue });
+  var refValue = url.split("=")[1]; // [/myroom/?ref, ref값]
+  res.render("sendmsg", { url: "/post/?ref=" + refValue });
 });
 
-router.post('/post', async (req, res) => {
+router.post("/post", async (req, res) => {
   try {
     var url = req.url; // /myroom/?ref=...
-    var refValue = url.split('=')[1]; // [/myroom/?ref, ref값]
+    var refValue = url.split("=")[1]; // [/myroom/?ref, ref값]
 
     const post = await Message.create({
       sender: req.body.sendername,
@@ -77,7 +81,7 @@ router.post('/post', async (req, res) => {
     });
 
     let query =
-      'select sum(price) as total from Message join Image on Message.imageId=Image.id where Message.ref=:ref';
+      "select sum(price) as total from Message join Image on Message.imageId=Image.id where Message.ref=:ref";
     let sum = await db.sequelize.query(query, {
       replacements: { ref: refValue },
       type: db.sequelize.QueryTypes.SELECT,
@@ -95,13 +99,13 @@ router.post('/post', async (req, res) => {
       }
     );
 
-    res.redirect('/complete');
+    res.redirect("/complete");
   } catch (error) {
     console.error(error);
   }
 });
 
-router.get('/complete', (req, res) => {
-  res.render('complete');
+router.get("/complete", (req, res) => {
+  res.render("complete");
 });
 module.exports = router;
